50f37990f4e8605f6fd2be249ec9d1ffbf796729
Alec Hipshear <alechipshear@gmail.com>
2014-02-12 15:09:32 -0500

    Generate a ton of data to show how caching works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+1.upto(1000) do |product_number|
+  Product.create name: "PRODUCT #{product_number}"
+end
+
